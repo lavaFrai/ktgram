c3d4e3c09927ba20d6d196e1bdf4740a43a979dc
lavafrai <lavafrai@yandex.ru>
2024-06-08 03:38:59 +0300

    Trying to publish jitpack

--- ktgram/build.gradle.kts
+++ ktgram/build.gradle.kts
@@ -5,11 +5,13 @@ buildscript {
 }
 
 plugins {
+    id("maven-publish")
     kotlin("jvm") version "1.9.23"
     kotlin("plugin.serialization") version "2.0.0"
 }
 
-version = "1.0.0"
+group = "ru.lavafrai.ktgram"
+version = "0.0.1"
 val ktor_version: String by project
 
 repositories {
@@ -27,4 +29,14 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
     implementation("org.slf4j:slf4j-simple:2.0.13")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("Maven") {
+            groupId = group as String
+            version = version as String
+            from(components["kotlin"])
+        }
+    }
 }
