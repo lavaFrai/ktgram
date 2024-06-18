7329508eb75fe11a2c1fa5f4edcd530b504bf8a3
lavafrai <lavafrai@yandex.ru>
2024-06-18 19:14:49 +0300

    jitpack gradle settings fix

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,17 @@ plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
 }
 rootProject.name = "ktgram"
-include(":ktgram")
-include(":examples:echo")
-include(":examples:filters")
-include(":examples:test")
-include(":examples:files")
-include(":examples:mediaGroup")
-include(":examples:inlineKeyboard")
-include(":examples:payments")
-include(":examples:inline")
-include(":examples:stateMachine")
-include(":examples:full")
+
+if (!System.getenv().containsKey("JITPACK")) include(":ktgram")
+else {
+    include(":examples:echo")
+    include(":examples:filters")
+    include(":examples:test")
+    include(":examples:files")
+    include(":examples:mediaGroup")
+    include(":examples:inlineKeyboard")
+    include(":examples:payments")
+    include(":examples:inline")
+    include(":examples:stateMachine")
+    include(":examples:full")
+}
